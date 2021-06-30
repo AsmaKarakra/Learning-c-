3045854419c187c24fa5a88789c101737fab2d57
Asma Karakra <78242119+AsmaKarakra@users.noreply.github.com>
2021-06-29 20:37:48 -0700

    Create 5.Working With Numbers.cpp

---
+++ 5.Working With Numbers.cpp
@@ -0,0 +1,44 @@
+
+// Working with Numbers 
+
+
+#include <iostream>
+
+using namespace std; 
+
+int main() 
+
+{
+	
+	int wnum = 5; 
+	
+	double dnum = 5.5; 
+	
+	wnum++; // incrememnts wnum by 1
+	
+	
+	
+	return 0; 
+	
+} 
+
+
+
+// using bulit in libraries 
+
+#include <iostream> 
+#include <cmath> 
+
+using namespace std; 
+
+int main() 
+
+{
+	
+	cout << pow(3,3); 
+	
+	return 0; 
+	
+} 
+
+// output = 3^3 which is 27 
