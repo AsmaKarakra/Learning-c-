8c01a2589d736c8b5c417d517a74719e4acb0aa9
Asma Karakra <78242119+AsmaKarakra@users.noreply.github.com>
2021-06-29 19:41:23 -0700

    Create 3.Data Types.cpp

---
+++ 3.Data Types.cpp
@@ -0,0 +1,37 @@
+
+// Data Types in C++ 
+
+#include <iostream>
+
+
+using namespace std; 
+
+int main() 
+
+{
+	
+	// represent one single character - single qoutation marks only 
+	char grade = 'A' 
+	
+	
+	// String - a string of characters 
+	string phrase = "Giraffe Academy"; 
+	
+	
+	// numbers: whole numbers and decimals 
+	int age = 22; 
+	
+	float weight = 132.8
+	
+	// more deciaml points than a float 
+	double gpa = 3.2 
+	
+	//booleans value - true or false 
+	bool isMale = false; 
+	
+	
+	
+	return 0; 
+	
+} 
+
