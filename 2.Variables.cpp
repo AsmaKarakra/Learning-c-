351a4d54f28a841f1d324226ce03ae248f0d7402
Asma Karakra <78242119+AsmaKarakra@users.noreply.github.com>
2021-06-29 19:27:57 -0700

    Create 2.Variables.cpp

---
+++ 2.Variables.cpp
@@ -0,0 +1,53 @@
+
+// Variables in C++ 
+
+
+// so we have the following story
+
+#include <iostream>
+
+using namespace std; 
+
+int main() 
+
+{
+	cout << "There once was a man named george" << endl;
+	cout << "He was 35 years old!" << endl;
+	cout << "He liked the name George" << endl;
+	cout << "but He did not like being 35" << endl;
+
+		return 0; 
+} 
+
+
+// We can store the character's name and age in a variable 
+
+
+/*  <data type>  <data type name>  = <value>  */ 
+
+string characterName = "John"; 
+
+int characterAge; 
+
+characterAge= 39; 
+
+
+// Adding variable into print statement 
+
+#include <iostream>
+
+using namespace std; 
+
+int main() 
+
+{
+	cout << "There once was a man named" << characterName << endl;
+	cout << "He was " << characterAge <<  "years old!" << characterName << endl;
+	cout << "He liked the name" << endl;
+	cout << "but He did not like being" << characterAge << endl;
+
+		return 0; 
+} 
+
+
+// you can modify the variables half way through the story by inserting the values within the places in your program
