d7f756f4cad9e3609e0c4c8aeed18b7c0c4fc82d
Asma Karakra <78242119+AsmaKarakra@users.noreply.github.com>
2021-07-01 19:51:58 -0700

    Create Mad Libs Game.cpp

---
+++ Projects/Mad Libs Game.cpp
@@ -0,0 +1,35 @@
+
+// Building a mad libs Game 
+
+#include <iostream> 
+#include <cmath> 
+
+using namepsace std; 
+
+int main() 
+
+{	
+
+	string color, pluralNoun, celebrity; 
+	
+	cout << "Enter a color:"; 
+	getline(cin, color); 
+	
+	cout << "Enter a plural noun: " ; 
+	getline(cin, pluralNoun); 
+	
+	cout << "Enter a celebrity: " ; 
+	getline(cin, celebrity); 
+
+
+	cout << "Roses are" << color << endl; 
+	
+	count << pluralnoun << "are blue" << endl; 
+	
+	cout << "I love << celebrity << endl; 
+	
+		
+	return 0; 
+	
+} 
+
