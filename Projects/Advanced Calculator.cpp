680f8d684b2061af031fba84b30b92ea69c9fe06
Asma Karakra <78242119+AsmaKarakra@users.noreply.github.com>
2021-07-02 21:50:13 -0700

    Create Advanced Calculator.cpp

---
+++ Projects/Advanced Calculator.cpp
@@ -0,0 +1,55 @@
+
+// Building a good calcuator // 
+
+
+#include <isotream> 
+
+
+using namespace std;  
+
+int main () 
+
+{ 
+
+int num1, num2; 
+
+char op; 
+
+
+cout << "Enter first number: "; 
+
+cin >> num1; 
+
+
+
+cout << "Enter second number: "; 
+
+cin >> num2; 
+
+
+
+
+cout << "Enter Your opreator: "; 
+
+cin >> op; 
+
+
+
+if (op == "+") {
+	
+	result = num1 + num 2; 
+} else if ( op == "-") {
+	
+result num1 - num2 } 
+
+else {
+cout "Invalid opreator has been entered!" 
+
+} 
+
+cout << result; 
+
+return 0; 
+
+
+} 
