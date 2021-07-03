e63369ee63a433a34c2375a4a9058febba921d3d
Asma Karakra <78242119+AsmaKarakra@users.noreply.github.com>
2021-07-03 08:16:45 -0700

    Create 13.while Loops.cpp

---
+++ 13.while Loops.cpp
@@ -0,0 +1,41 @@
+
+/* while loop 
+
+
+while( <Insert Condition > ) {
+	
+	
+	code to execuate until condition is not true 
+} 
+
+
+*/ 
+
+
+#include <iostream> 
+
+using namespace std; 
+
+int main() 
+
+{ 
+
+
+
+int index = 1; 
+
+while( index <= 5) {
+	
+	
+	cout << index << endl; 
+	index++; 
+	
+} 
+
+//output will be 1 , 2 , 3, 4, 5
+
+return 0; 
+
+
+} 
+
